f2a41c73c0a8eb25dcaff1ce5a3b610d35b014b6
magdakarcz <127899614+magdakarcz@users.noreply.github.com>
2023-11-11 17:54:00 +0100

    Add files via upload

---
+++ zadanie4/src/main/java/com/jsfcourse/calc/KredytBB.java
@@ -0,0 +1,71 @@
+package com.jsfcourse.calc;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
+public class KredytBB {
+	private String x;
+	private String y;
+	private String z;
+	private Double result;
+
+	@Inject
+	FacesContext ctx;
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+
+	public String getY() {
+		return y;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+	
+	
+	public String getZ() {
+		return z;
+	}
+
+	public void setZ(String z) {
+		this.z = z;
+	}
+
+	public Double getResult() {
+		return result;
+	}
+	//public void setResult(Double result) {
+	//	this.result = result;
+	//}
+
+	public String calc() {
+		try {
+			double x = Double.parseDouble(this.x);
+			double y = Double.parseDouble(this.y);
+			double z = Double.parseDouble(this.x);
+
+            result = x/y/12;
+        	result = result+result*z/100;
+
+        	ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Operacja wykonana poprawnie", null));
+			return "showresult"; 
+		} catch (Exception e) {
+			ctx.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Błąd podczas przetwarzania parametrów", null));
+			return null; 
+		}
+	}
+	public String info() {
+		return "info"; 
+	}
+}
